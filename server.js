46a7b3c6321e6642cca98f838554ac3c8805141e
hania kanwal <haniakanwal239@gmail.com>
2026-04-10 08:23:47 +0500

    vercel changes

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-require("dotenv").config()
-const app = require("./src/app")
-const connectDB = require("./src/config/database")
+require("dotenv").config();
+const app = require("./src/app");
+const connectDB = require("./src/config/database");
 
+connectDB();
 
-connectDB()
-
-
-app.listen(3000,()=>{
+// For local development
+if (process.env.NODE_ENV !== "production") {
+  app.listen(3000, () => {
     console.log("running on port 3000");
-    
-})
+  });
+}
+
+// Export for Vercel (serverless)
+module.exports = app;
